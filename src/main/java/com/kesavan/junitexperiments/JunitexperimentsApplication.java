de65ee10d84c3e70afaf0438e7c97f160154086c
kesavanmurali <k7mraj@gmail.com>
2023-06-25 13:49:34 +0530

    Initializing spring boot project

---
+++ src/main/java/com/kesavan/junitexperiments/JunitexperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.kesavan.junitexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitexperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitexperimentsApplication.class, args);
+	}
+
+}
